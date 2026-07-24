219d8e859cc451fc40b5bd561025a68dd7d558bb
bvslahari007 <bvslahari@gmail.com>
2026-07-24 17:37:25 +0000

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root(): #no input from the client is taken here
-	return {"message": "Hello, world"} # fastapi will convert dict to json
+	return {"name": "Task API", "version": "1.0", "endpoints":["/tasks"]} # fastapi will convert dict to json
 
+@app.get("/health")
+def health():
+	return {"status": "ok"}
