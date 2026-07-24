651cd2c03650ba0d60102ebd04958ab9474f329f
bvslahari007 <bvslahari@gmail.com>
2026-07-24 18:16:46 +0000

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
+tasks = [
+    {"id": 1, "title": "Complete WAF assignments", "done": False},
+    {"id": 2, "title": "GIT repo create for proj: Portfolio app", "done": True},
+    {"id": 3, "title": "Clean the table", "done": False}
+]
 
 app = FastAPI()
 
 @app.get("/")
-def read_root(): #no input from the client is taken here
-	return {"name": "Task API", "version": "1.0", "endpoints":["/tasks"]} # fastapi will convert dict to json
+def read_root(): 
+	return {"name": "Task API", "version": "1.0", "endpoints":["/tasks"]} 
 
 @app.get("/health")
 def health():
 	return {"status": "ok"}
+
+@app.get("/tasks")
+def get_tasks():
+	return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+	for task in tasks:
+		if task['id'] == task_id:
+			return task
+	raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
